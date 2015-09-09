1dcdec5487192104d5ead4b865e22330759df156
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-09-09 18:54:58 +0300

    POST request for /messages/ handler added.

--- routes/messages.js
+++ routes/messages.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.send('Access denied to messages list');
+});
+
+router.post('/', function (req, res) {
+    res.send(JSON.stringify(req.body));
 });
 
 module.exports = router;
