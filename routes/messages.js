1d1af1ba346e8fc19c705a6a58f390e90495fc2a
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-09-10 13:23:32 +0300

    Bug fixed - losses of messages data.

--- routes/messages.js
+++ routes/messages.js
@@ -12,9 +12,11 @@ router.post('/', function (req, res) {
     //res.send(JSON.stringify(req.body));
     if (typeof req.body.channel !== 'undefined' && req.body.channel){
         var channelName = 'channel:' + req.body.channel;
-        //publisher.publish(channelName , req.body);
-        publisher.publish(channelName , req.body);
-        console.log('REST API publishes new message to channel', channelName, req.body);
+        var messageData = req.body;
+        delete messageData.channel;
+        var messageString = JSON.stringify(messageData);
+        console.log('REST API publishes new message to channel', channelName, messageString);
+        publisher.publish(channelName , messageString);
         res.send('Message published to channel [' + channelName + ']');
     } else {
         res.send('Channel must be specified.');
