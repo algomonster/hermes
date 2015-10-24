0e1a15c856b0389e0d3a3f9df21b888cdaa2ecaf
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-10-24 23:12:49 +0200

    First mocha test has been implemented. Issue #1 closed.

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Run server using default settings:
 ```
 node ./bin/www
 ```
+
+### Run tests
+
+Install mocha
+```
+sudo npm install -g mocha
+```
+
+mocha tests/mocha tests/01-index-page-is-working.js
---
+++ tests/01-index-page-is-working.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var superagent = require('superagent');
+var server = require('../app.js');
+var status = require('http-status');
+
+describe('/messages', function() {
+
+    before(function() {
+        server.listen(4000);
+    });
+
+    after(function() {
+    });
+
+    it('Returns 422 if channel parameter is missing', function(done) {
+        superagent.post('http://localhost:4000/messages').end(function(err, res) {
+            assert.equal(res.status, status.UNPROCESSABLE_ENTITY);
+            var result = JSON.parse(res.text);
+            assert.deepEqual({ error: 'Channel must be specified' }, result);
+            done();
+        });
+    });
+});
