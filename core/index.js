090476d7cbb968602df91c41d39e4c30cc4d7c43
Alexander Emelyanov <alexander.emelyanov@outlook.com>
2016-04-23 14:19:34 +0200

    Core refactoring.

--- core/index.js
+++ core/index.js
@@ -2,40 +2,39 @@
  *
  */
 var FifoArray = require('fifo-array');
+var cfg = null;
 var history = {};
-var i = 0;
 
 function Core(config) {
-
-
-    var index = ++i;
-    console.log('Core instance #' + index + ' created.');
-
-    /**
-     * Process messages
-     *
-     * @param message
-     */
-    this.process = function (message) {
-        message.serverTime = (new Date()).getTime();
-        if (!(message.channel in history)){
-            var historyDepth = 10;
-            try {
-                historyDepth = config.core.history.maxDepth;
-            } catch (e) {}
-            history[message.channel] = new FifoArray(historyDepth);
-        }
-        console.log('Core got message for processing', message);
-        history[message.channel].push(message);
-        return message;
-    };
-
-    this.getHistory = function (channel) {
-        if (!(channel in history)){
-            return [];
-        }
-        return history[channel];
-    };
+    if (cfg == null) {
+        cfg = config;
+    }
 }
 
 module.exports = Core;
+
+/**
+ * Process messages
+ *
+ * @param message
+ */
+Core.prototype.process = function (message) {
+    message.serverTime = (new Date()).getTime();
+    if (!(message.channel in history)){
+        var historyDepth = 10;
+        try {
+            historyDepth = cfg.core.history.maxDepth;
+        } catch (e) {}
+        history[message.channel] = new FifoArray(historyDepth);
+    }
+    console.log('Core got message for processing', message);
+    history[message.channel].push(message);
+    return message;
+};
+
+Core.prototype.getHistory = function (channel) {
+    if (!(channel in history)){
+        return [];
+    }
+    return history[channel];
+};
